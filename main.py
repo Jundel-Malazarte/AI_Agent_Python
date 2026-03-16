12eb79026eaf631eea1c2bebe78da8f28199d226
Jundel-Malazarte <jundelmalazarte348@gmail.com>
2026-03-16 23:03:52 +0800

    fixing langchain_core.output_parsers import PydanticOutputParser.

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 
 load_dotenv()
 # Initialize the OpenAI and Anthropic chat models
 # openai_llm = ChatOpenAI(model="gpt-4-0613")
 llm = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+
+
+
+
+
